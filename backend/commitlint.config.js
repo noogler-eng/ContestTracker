ea8a141159e7c1b371528b1bbbf2c7c11bff16f9
noogler-eng <sharadpoddar10102003@gmail.com>
2025-03-31 01:49:15 +0530

    try for husky

--- backend/commitlint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'subject-empty': [2, 'never'],  // Prevent empty commit subjects
-      'type-empty': [2, 'never'],     // Prevent empty commit types
-      'header-max-length': [2, 'always', 100]  // Allow longer messages (default 72)
-    }
-  };
