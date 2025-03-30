dcd8e6c4f4de78ae0a5b9175579c57e2151da39d
noogler-eng <sharadpoddar10102003@gmail.com>
2025-03-31 01:41:19 +0530

    add

--- backend/commitlint.config.js
+++ backend/commitlint.config.js
@@ -1,2 +1,8 @@
-// commitlint.config.js
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'subject-empty': [2, 'never'],  // Prevent empty commit subjects
+      'type-empty': [2, 'never'],     // Prevent empty commit types
+      'header-max-length': [2, 'always', 100]  // Allow longer messages (default 72)
+    }
+  };
